6410e0033fde7f49ee240d7fc277671a6bef7ce5
Yasser Farid <111323659+Yasserxm@users.noreply.github.com>
2025-09-27 15:44:10 -0300

    Adição completa do repositório Java.

---
+++ CSP_Java/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("org.choco-solver:choco-solver:4.10.9")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
