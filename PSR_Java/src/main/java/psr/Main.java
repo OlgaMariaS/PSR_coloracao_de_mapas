433979efccd5c8864811fd2313e8ac2c2c5ad972
Yasser Farid <111323659+Yasserxm@users.noreply.github.com>
2025-09-27 16:21:15 -0300

    Alterações nos prints dos textos.

--- PSR_Java/src/main/java/psr/Main.java
+++ PSR_Java/src/main/java/psr/Main.java
@@ -7,9 +7,9 @@ public class Main {
         String[] paths  = {"instancia_facil.json", "instancia_media.json", "instancia_dificil.json", "instancia_muito_dificil.json"};
 
         for (int i = 0; i < paths.length; i++) {
-            System.out.println("\n📌 ==========================================");
-            System.out.println("📌 Executando instância: " + nomes[i]);
-            System.out.println("📌 ==========================================");
+            System.out.println("\n ==========================================");
+            System.out.println("Executando instância: " + nomes[i]);
+            System.out.println("==========================================");
 
             try {
                 Instancia inst = LeitorInstancia.carregar(paths[i]);
@@ -21,15 +21,15 @@ public class Main {
 
                 System.out.println("--- Resultados ---");
                 if (r.atribuicaoDeCores != null) {
-                    System.out.println("✅ Solução encontrada:");
+                    System.out.println("Solução encontrada:");
                     r.atribuicaoDeCores.keySet().stream().sorted()
                             .forEach(v -> System.out.println("   " + v + ": " + r.atribuicaoDeCores.get(v)));
                 } else {
-                    System.out.println("❌ Nenhuma solução encontrada.");
+                    System.out.println("Nenhuma solução encontrada.");
                 }
 
-                System.out.printf("⏱️  Tempo de execução: %d ms%n", r.tempoExecucaoMs);
-                System.out.printf("📉 Contagem de falhas/retornos: %d%n", r.totalFalhas);
+                System.out.printf("Tempo de execução: %d ms%n", r.tempoExecucaoMs);
+                System.out.printf("Contagem de falhas/retornos: %d%n", r.totalFalhas);
 
             } catch (Exception e) {
                 System.out.println("Erro ao processar " + nomes[i] + ": " + e.getMessage());
